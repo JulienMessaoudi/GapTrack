ac2a3aa2955949d5875f5112df1aa0bbb4397f71
Julien <julien.messaoudi@edu.esiee.fr>
2026-07-17 16:51:22 +0200

    vercel.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,52 +10,7 @@ export default defineConfig({
   integrations: [react()],
 
   security: {
-    csp: {
-      algorithm: "SHA-256",
-
-      scriptDirective: {
-        resources: [
-          {
-            resource: "'self'",
-            kind: "element",
-          },
-          {
-            resource: "'none'",
-            kind: "attribute",
-          },
-        ],
-      },
-
-      styleDirective: {
-        resources: [
-          {
-            resource: "'self'",
-            kind: "element",
-          },
-          {
-            resource: "'unsafe-inline'",
-            kind: "attribute",
-          },
-        ],
-      },
-
-      directives: [
-        "default-src 'self'",
-        "base-uri 'self'",
-        "object-src 'none'",
-        "frame-src 'none'",
-        "form-action 'self'",
-
-        "img-src 'self' data: blob:",
-        "font-src 'self' data:",
-
-        "connect-src 'self' https://uezjmzkjrrqzxbkcjzwu.supabase.co wss://uezjmzkjrrqzxbkcjzwu.supabase.co",
-
-        "worker-src 'self' blob:",
-        "manifest-src 'self'",
-        "upgrade-insecure-requests",
-      ],
-    },
+    csp: false,
   },
 
   vite: {
