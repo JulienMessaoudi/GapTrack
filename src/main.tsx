73db348ae46fc0f76913f991b246135adc8813e8
Julien <julien.messaoudi@edu.esiee.fr>
2026-07-19 20:28:03 +0200

    Corrige l'ordre de chargement du thème

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
-﻿import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+﻿import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import "./styles/theme.css";
 
-import { Toaster } from 'sonner'   // ðŸ‘ˆ ajout
+import { Toaster } from "sonner";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
